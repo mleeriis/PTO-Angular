5526f48b5fb8add4ec30c31dcc275c09f5c6373f
mleeriis <mlee@riis.com>
2019-08-26 10:12:40 -0400

    Switching PTO Front-End to Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ViewRequestsComponent } from './view-requests/view-requests.component';
+import { MakeRequestsComponent } from './make-requests/make-requests.component';
+import { EmployeeViewComponent } from './employee-view/employee-view.component';
+import { HrViewComponent } from './hr-view/hr-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ViewRequestsComponent,
+    MakeRequestsComponent,
+    EmployeeViewComponent,
+    HrViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/hr-view/hr-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hr-view',
+  templateUrl: './hr-view.component.html',
+  styleUrls: ['./hr-view.component.css']
+})
+export class HrViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
