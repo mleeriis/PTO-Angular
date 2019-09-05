ad62169be8d278f83777088a59a9e86e5f549379
mleeriis <mlee@riis.com>
2019-09-05 16:38:44 -0400

    Navigation and authentication work, but router navigation is not working

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -33,22 +33,22 @@ export class AuthService {
         'email': email,
         'password': password
       }, this.httpOptions).subscribe(responseData => {
-      console.log(responseData);
+        this.loggedIn = !responseData;
     });
 
-    if (email === null || password === null) {
-      this.loggedIn = false;
-    } else if (email === 'mlee@riis.com' && password === 'password') {
-      this.loggedIn = true;
-      this.employeeType = 2;
-      this.employeeId = 2;
-      this.employeeName = 'Maria Lee';
-    } else if (email === 'jmarcotte@riis.com' && password === 'password') {
-      this.loggedIn = true;
-      this.employeeType = 1;
-      this.employeeId = 1;
-      this.employeeName = 'Jillian Marcotte';
-    }
+    // if (email === null || password === null) {
+    //   this.loggedIn = false;
+    // } else if (email === 'mlee@riis.com' && password === 'password') {
+    //   this.loggedIn = true;
+    //   this.employeeType = 2;
+    //   this.employeeId = 2;
+    //   this.employeeName = 'Maria Lee';
+    // } else if (email === 'jmarcotte@riis.com' && password === 'password') {
+    //   this.loggedIn = true;
+    //   this.employeeType = 1;
+    //   this.employeeId = 1;
+    //   this.employeeName = 'Jillian Marcotte';
+    // }
     return this.loggedIn;
   }
 
