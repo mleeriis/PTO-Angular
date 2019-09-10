d32ccadfabb50ebaed940fa5a37c9ee3fc4acff1
mleeriis <mlee@riis.com>
2019-09-10 11:03:30 -0400

    Add unit tests for requests service

---
+++ src/app/services/requests.service.spec.ts
@@ -0,0 +1,46 @@
+import {TestBed, inject} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {RequestsService} from './requests.service';
+
+describe('RequestsService', () => {
+  let httpTestingController: HttpTestingController;
+  let service: RequestsService;
+
+  const dummyRequest = {
+    employeeID: 1,
+    startDate: '2019-09-10',
+    endDate: '2019-09-11',
+    status: 2
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [RequestsService],
+      imports: [
+        HttpClientTestingModule
+      ],
+    });
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(RequestsService);
+  });
+
+  afterEach(inject([HttpTestingController], () => {
+    httpTestingController.verify();
+  }));
+
+  it('should be created', () => {
+    expect(RequestsService).toBeTruthy();
+  });
+
+  it('#createPtoRequest(), should send POST request to API', () => {
+    service.createPtoRequest(dummyRequest).subscribe((resData) => {
+      expect(resData).toEqual(dummyRequest);
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:8080/pto');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.responseType).toEqual('json');
+
+    req.flush(dummyRequest);
+  });
+});
